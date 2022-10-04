a07c6ab4f1266e28179e9060732088274606cea1
DanilaTru <dan.trurk@gmail.com>
2022-10-04 14:05:13 +0300

    trying map distance

---
+++ distance.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+// const cors = require('cors');  //  npm install cors
+const fetch = require('cross-fetch');  // npm install --save cross-fetch
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+let url = 'https://www.rasstoyanie.com/route.json?stops=Москва|Астана'
+
+const getDistance = async (url) => {
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data.distance);
+    return data.distance;
+  }
+
+  console.log(getDistance(url));
+
+
+
+app.listen(3005,  async () => {
+    try {
+        // await sequelize.authenticate();
+        // console.log('Соедение с бд установлено');
+        console.log(`Сервак запущен на 3005 порту`);
+    } catch (error) {
+        console.log('Сервак или база легли...', error);
+    }
+});
+
+
+module.exports = getDistance
