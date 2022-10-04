49f24aa3ac3023bca883902b038ff29a32fffc6d
DanilaTru <dan.trurk@gmail.com>
2022-10-04 19:47:51 +0300

    trying yandex map + process integrate in project

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -69,9 +69,7 @@ module.exports = function Layout({ user, children }) {
           integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
           crossOrigin="anonymous"
         />
-        <script defer src="https://api-maps.yandex.ru/2.1/?apikey=16ae44fe-6ff1-4dee-84fe-86c6dbed24d3&lang=ru_RU" type="text/javascript" />
         <script src="/js/application.js" />
-        <script defer src="/js/yandexMap.js" />
       </body>
     </html>
   );
