b5bc348851c8043dd513fb841105b0b4f5a9edba
DanilaTru <dan.trurk@gmail.com>
2022-10-04 17:03:40 +0300

    trying yandex map

--- distance/yandex.js
+++ distance/yandex.js
@@ -25,136 +25,26 @@ function init () {
         toEnabled: true,
         to: `${moscow}, шоссе Энтузиастов, 52`,
     })
+
+    multiRoutePromise.then(function(multiRoute) {
+        // Подписка на событие обновления мультимаршрута.
+        multiRoute.model.events.add('requestsuccess', function() {
+            // Получение ссылки на активный маршрут.
+            let activeRoute = multiRoute.getActiveRoute();
+            // Когда панель добавляется на карту, она
+            // создает маршрут с изначально пустой геометрией. 
+            // Только когда пользователь выберет начальную и конечную точки,
+            // маршрут будет перестроен с непустой геометрией.
+            // Поэтому для избежания ошибки нужно добавить проверку,
+            // что маршрут не пустой.
+            if (activeRoute) {
+                // Вывод информации об активном маршруте.
+                console.log("Длина: " + activeRoute.properties.get("distance").text);
+                console.log("Время прохождения: " + activeRoute.properties.get("duration").text);
+            }
+        });
+    }, function (err) {
+      console.log(err); 
+    });
 }
-    
-// ymaps.modules.define('MultiRouteCustomView', [
-//     'util.defineClass'
-// ], function (provide, defineClass) {
-//     // Класс простого текстового отображения модели мультимаршрута.
-//     function CustomView (multiRouteModel) {
-//         this.multiRouteModel = multiRouteModel;
-//         // Объявляем начальное состояние.
-//         this.state = "init";
-//         this.stateChangeEvent = null;
-//         // Элемент, в который будет выводиться текст.
-//         this.outputElement = $('<div></div>').appendTo('#viewContainer');
-
-//         this.rebuildOutput();
-
-//         // Подписываемся на события модели, чтобы
-//         // обновлять текстовое описание мультимаршрута.
-//         multiRouteModel.events
-//             .add(["requestsuccess", "requestfail", "requestsend"], this.onModelStateChange, this);
-//     }
-
-//     // Таблица соответствия идентификатора состояния имени его обработчика.
-//     CustomView.stateProcessors = {
-//         init: "processInit",
-//         requestsend: "processRequestSend",
-//         requestsuccess: "processSuccessRequest",
-//         requestfail: "processFailRequest"
-//     };
-
-//     // Таблица соответствия типа маршрута имени его обработчика.
-//     CustomView.routeProcessors = {
-//         "driving": "processDrivingRoute",
-//         "masstransit": "processMasstransitRoute",
-//         "pedestrian": "processPedestrianRoute"
-//     };
-
-//     defineClass(CustomView, {
-//         // Обработчик событий модели.
-//         onModelStateChange: function (e) {
-//             // Запоминаем состояние модели и перестраиваем текстовое описание.
-//             this.state = e.get("type");
-//             this.stateChangeEvent = e;
-//             this.rebuildOutput();
-//         },
-
-//         rebuildOutput: function () {
-//             // Берем из таблицы обработчик для текущего состояния и исполняем его.
-//             var processorName = CustomView.stateProcessors[this.state];
-//             this.outputElement.html(
-//                 this[processorName](this.multiRouteModel, this.stateChangeEvent)
-//             );
-//         },
-
-//         processInit: function () {
-//             return "Инициализация ...";
-//         },
-
-//         processRequestSend: function () {
-//             return "Запрос данных ...";
-//         },
-
-//         processSuccessRequest: function (multiRouteModel, e) {
-//             var routes = multiRouteModel.getRoutes(),
-//                 result = ["Данные успешно получены."];
-//             if (routes.length) {
-//                 result.push("Всего маршрутов: " + routes.length + ".");
-//                 for (var i = 0, l = routes.length; i < l; i++) {
-//                     result.push(this.processRoute(i, routes[i]));
-//                 }
-//             } else {
-//                 result.push("Нет маршрутов.");
-//             }
-//             return result.join("<br/>");
-//         },
-
-//         processFailRequest: function (multiRouteModel, e) {
-//             return e.get("error").message;
-//         },
-
-//         processRoute: function (index, route) {
-//             // Берем из таблицы обработчик для данного типа маршрута и применяем его.
-//             var processorName = CustomView.routeProcessors[route.properties.get("type")];
-//             return (index + 1) + ". " + this[processorName](route);
-//         },
-
-//         processDrivingRoute: function (route) {
-//             var result = ["Автомобильный маршрут."];
-//             result.push(this.createCommonRouteOutput(route));
-//             return result.join("<br/>");
-//         },
-
-//         processMasstransitRoute: function (route) {
-//             var result = ["Маршрут на общественном транспорте."];
-//             result.push(this.createCommonRouteOutput(route));
-//             result.push("Описание маршута: <ul>" + this.createMasstransitRouteOutput(route) + "</ul>");
-//             return result.join("<br/>");
-//         },
-
-//         processPedestrianRoute: function (route) {
-//             var result = ["Пешеходный маршрут."];
-//             result.push(this.createCommonRouteOutput(route));
-//             return result.join("<br/>");
-//         },
-
-//         // Метод, формирующий общую часть описания для всех типов маршрутов.
-//         createCommonRouteOutput: function (route) {
-//             return "Протяженность маршрута: " + route.properties.get("distance").text + "<br/>" +
-//                 "Время в пути: " + route.properties.get("duration").text;
-//         },
-
-//         // Метод, строящий список текстовых описаний для
-//         // всех сегментов маршрута на общественном транспорте.
-//         createMasstransitRouteOutput: function (route) {
-//             var result = [];
-//             for (var i = 0, l = route.getPaths().length; i < l; i++) {
-//                 var path = route.getPaths()[i];
-//                 for (var j = 0, k = path.getSegments().length; j < k; j++) {
-//                     result.push("<li>" + path.getSegments()[j].properties.get("text") + "</li>");
-//                 }
-//             }
-//             return result.join("");
-//         },
-
-//         destroy: function () {
-//             this.outputElement.remove();
-//             this.multiRouteModel.events
-//                 .remove(["requestsuccess", "requestfail", "requestsend"], this.onModelStateChange, this);
-//         }
-//     });
-
-//     provide(CustomView);
-// });
+ 
