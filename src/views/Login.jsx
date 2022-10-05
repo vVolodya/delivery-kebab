305e26788da22aa893ff4140749d623e6d88ea3c
Ellina Shcherbaeva <s.ellina2020@gmail.com>
2022-10-05 19:06:36 +1100

    Rego and login styling

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -4,16 +4,22 @@ const Layout = require('./Layout');
 module.exports = function Login() {
   return (
     <Layout>
-      <form method="POST" action="/login">
-        <div>
-          <label>Email address</label>
-          <input type="email" name="email" />
+      <h1>Login</h1>
+      <form method="POST" action="/login" encType="multipart/form-data" className="w-25 p-3">
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="login-email">Email address</span>
+          </div>
+          <input type="email" name="email" className="form-control" placeholder="Email" aria-label="Email" aria-describedby="login-email" />
         </div>
-        <div>
-          <label>Password</label>
-          <input type="password" name="password" />
+
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="login-password">Password</span>
+          </div>
+          <input type="password" name="password" className="form-control" placeholder="Password" aria-label="Password" aria-describedby="login-password" />
         </div>
-        <button type="submit">Login</button>
+        <button type="submit" className="btn btn-primary">Login</button>
       </form>
     </Layout>
   );
